63c909952112c903a677ea291c6c8d825ad7bd7c
pro100proger <nazar4075066@gmail.com>
2023-02-02 19:28:02 +0200

    create bootstrap project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
 
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
